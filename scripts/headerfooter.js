351cdc14c97f24bc905eeee9c243249340a1f097
rterrade <rterrade@pce-test>
2024-10-15 09:04:37 +0200

    feat: add color variables

--- scripts/headerfooter.js
+++ scripts/headerfooter.js
@@ -1,7 +1,7 @@
 export async function loadFooterHeader() {
     // Load components
-    const footerHTML = await fetch('components/footer.html').then(response => response.text());
-    const headerHTML = await fetch('components/header.html').then(response => response.text());
+    const footerHTML = await fetch('components/footer').then(response => response.text());
+    const headerHTML = await fetch('components/header').then(response => response.text());
 
     let header = document.getElementById("header");
     let footer = document.getElementById("footer");
