4c2b7fde57c4a9a42b0fed0f1e8695813d9218a6
RatPeureux <unratpeureux@gmail.com>
2024-10-15 10:15:19 +0200

    init fonts et size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ["./pages/**/*.{html,js}"],
+    content: [
+        "./pages/**/*",
+    ],
     theme: {
-        extend: {},
+        fontFamily: {
+            'cormorant': ['"Cormorant-Bold"'],
+            'sans': ['Poppins'],
+        },
+        fontSize: {
+            'small': ['14px'],
+            'title': ['30px'],
+            'PACT': ['35px', {
+                letterSpacing: '10px',
+            }],
+        },
+        extend: {
+            colors: {
+                primary: '#F2771B',
+                secondary: '#00350D',
+                base100: '#F1F3F4',
+                base200: '#E0E0E0',
+                base300: '#CCCCCC',
+                neutre: '#000',
+                gris: '#828282',
+                bgBlur: "#F1F3F4/[0.75]", 
+            },
+        },
     },
     plugins: [],
 }
