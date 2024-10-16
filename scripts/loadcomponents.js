b6447ae66d3330720319482d34307e5f988e2b73
richard <zenpoxa@gmail.com>
2024-10-16 18:26:34 +0200

    feat: responsive for all offers, not cards

--- scripts/loadcomponents.js
+++ scripts/loadcomponents.js
@@ -5,21 +5,29 @@ async function loadFooterHeader() {
     const headerHTML = await fetch('./components/header.html').then(response => response.text());
     const menuHTML = await fetch('./components/menu.html').then(response => response.text());
 
-    let header = document.getElementById("header");
+    let headers = document.querySelectorAll("#header");
     let footer = document.getElementById("footer");
-    let menu = document.getElementById("menu");
-    
+    let menus = document.querySelectorAll("#menu");
+
     // Add text
-    header.innerHTML = headerHTML;
+    for (const header of headers) {
+        header.innerHTML = headerHTML;
+    }
     footer.innerHTML = footerHTML;
-    menu.innerHTML = menuHTML;
+    for (const menu of menus) {
+        menu.innerHTML = menuHTML;
+    }
     
     // Put active class to the given element (number in class)
-    let activeMenuItemIdx = parseInt(menu.classList.toString());
-    let allItems = menu.querySelector('div.all-items');
-    let activeItem = allItems.querySelector(`:nth-child(${activeMenuItemIdx})`);
-    if (activeItem) {
-        activeItem.classList.add('active');
+    for (const menu of menus) {
+        let activeMenuItemIdx = parseInt(menu.classList.toString());
+        let allItemsSections = menu.querySelectorAll('div.all-items');
+        for (const allItems of allItemsSections) {
+            let activeItem = allItems.querySelector(`:nth-child(${activeMenuItemIdx})`);
+            if (activeItem) {
+                activeItem.classList.add('active');
+            }
+        }
     }
 }
 
