d0c49dd1699aee4697cb4f87d0e0fcf7b4166ba5
BatFla <baptiste.frolla@etudiant.univ-rennes1.fr>
2024-10-23 14:14:16 +0200

    maj

--- html/php/connect_params.php
+++ html/php/connect_params.php
@@ -3,5 +3,5 @@ $server = 'servbdd.iutlan.etu.univ-rennes1.fr'; // ou '127.0.0.1' pour se connec
 $driver = 'pgsql'; 
 $dbname = 'pg_bfrolla'; // Remplacez par le nom de votre base 
 $user   = 'bfrolla'; // Remplacez par votre nom d'utilisateur 
-$pass   = 'Mygualba_205'; // Remplacez par votre mot de passe 
+$pass   = ''; // Remplacez par votre mot de passe 
 ?>
