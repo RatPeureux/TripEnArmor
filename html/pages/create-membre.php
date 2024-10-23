85fa387cc31ee2ca886b16344a3f5bdc4e7e4a91
RatPeureux <unratpeureux@gmail.com>
2024-10-23 13:36:04 +0200

    étoiles form

--- html/pages/create-membre.php
+++ html/pages/create-membre.php
@@ -168,11 +168,11 @@ $mdp = $_POST['mdp'];
             <input class="p-2 text-gris bg-base100 w-full h-12 mb-1.5 rounded-lg" type="email" id="mail" name="mail" title="Mon adresse mail" value="<?php echo $mail;?>" readonly>
             
             <!-- Champ pour le pseudonyme -->
-            <label class="text-small" for="pseudo">Pseudonyme<label class="text-rouge-logo"> *</label></label>
+            <label class="text-small" for="pseudo">Pseudonyme</label>
             <input class="p-2 bg-base100 w-full h-12 mb-1.5 rounded-lg" type="text" id="pseudo" name="pseudo" pattern="^(?:(\w+|\w+[\.\-_]?\w+)+" title="Saisir mon pseudonyme PACT" maxlength="16" required>
             
             <!-- Champs pour l'adresse -->
-            <label class="text-small" for="adresse">Adresse postale<label class="text-rouge-logo"> *</label></label>
+            <label class="text-small" for="adresse">Adresse postale</label>
             <input class="p-2 bg-base100 w-full h-12 mb-1.5 rounded-lg" type="text" id="adresse" name="adresse" pattern="\d{1,5}\s[\w\s.-]+$" title="Saisir mon adresse postale" maxlength="255" required>
             
             <label class="text-small" for="complement">Complément d'adresse postale</label>
@@ -180,17 +180,17 @@ $mdp = $_POST['mdp'];
             
             <div class="flex flex-nowrap space-x-3 mb-1.5">
                 <div class="w-28">
-                    <label class="text-small" for="code">Code postal<label class="text-rouge-logo"> *</label></label>
+                    <label class="text-small" for="code">Code postal</label>
                     <input class="text-right p-2 bg-base100 w-28 h-12 rounded-lg" type="text" id="code" name="code" pattern="^(0[1-9]|[1-8]\d|9[0-5]|2A|2B)\d{3}$" title="Saisir mon code postal" minlength="5" maxlength="5" oninput="number(this)" required>
                 </div>
                 <div class="w-full">
-                    <label class="text-small" for="ville">Ville<label class="text-rouge-logo"> *</label></label>
+                    <label class="text-small" for="ville">Ville</label>
                     <input class="p-2 bg-base100 w-full h-12 rounded-lg" type="text" id="ville" name="ville" pattern="^[a-zA-Zéèêëàâôûç\-'\s]+(?:\s[A-Z][a-zA-Zéèêëàâôûç\-']+)*$" title="Saisir ma ville" maxlength="50" required>
                 </div>
             </div>
 
             <!-- Champ pour le numéro de téléphone -->
-            <label class="text-small" for="num_tel">Téléphone<label class="text-rouge-logo"> *</label></label>
+            <label class="text-small" for="num_tel">Téléphone</label>
             <div class="w-full">
                 <input class="text-center p-2 bg-base100 w-36 h-12 mb-3 rounded-lg" type="tel" id="num_tel" name="num_tel" pattern="^0\d( \d{2}){4}" title="Saisir un numéro de téléphone" minlength="14" maxlength="14" oninput="formatTEL(this)" required>
             </div>
@@ -198,7 +198,7 @@ $mdp = $_POST['mdp'];
             <!-- Choix d'acceptation des termes et conditions -->
             <div class="mb-1.5 flex items-start">
                 <input class="mt-0.5 mr-1.5" type="checkbox" id="termes" name="termes" title="Accepter pour continuer" required>
-                <label class="text-small" for="termes">J’accepte les <u class="cursor-pointer">conditions d'utilisation</u> et vous confirmez que vous avez lu notre <u class="cursor-pointer">Politique de confidentialité et d'utilisation des cookies</u>.<label class="text-rouge-logo"> *</label></label>
+                <label class="text-small" for="termes">J’accepte les <u class="cursor-pointer">conditions d'utilisation</u> et vous confirmez que vous avez lu notre <u class="cursor-pointer">Politique de confidentialité et d'utilisation des cookies</u>.</label>
             </div>
 
             <!-- Messages d'erreurs -->
