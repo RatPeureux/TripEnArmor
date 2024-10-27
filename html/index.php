77355aeddf9b934ebb0cd776edd4eb23ec418d83
richard <zenpoxa@gmail.com>
2024-10-27 13:49:27 +0100

    feat

--- html/index.php
+++ html/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include dirname($_SERVER['DOCUMENT_ROOT']) . '/php-files/authentification.php';
 ?>
 
--- html/pages/components/header.php
+++ html/pages/components/header.php
@@ -2,7 +2,7 @@
     Composant du header pour les visiteurs / membres
     Pour l'ajouter, écrier la balise <div id='header'></div> dans votre code html
     (responsive)
--->
+    -->
 <header class="z-30 w-full bg-white flex justify-center p-4 h-20 border-b-2 border-black top-0">
     <div class="flex w-full justify-between items-center">
         <a href="" onclick="toggleMenu()" class="md:hidden">
@@ -15,8 +15,8 @@
             <a href="/pages/login-membre.php">
                 <i class="text-3xl fa-regular fa-user"></i>
             </a>
-            <?php
 
+            <?php
             include dirname($_SERVER['DOCUMENT_ROOT']) . '/php-files/authentification.php';
             if (activeLogout()) { ?>
                 <a href="/php/membre/logout.php" onclick="return confirmLogout(event)">
