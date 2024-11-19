4b4a9c16455335a0c8ee585ee6ebf404236bbaa7
RatPeureux <unratpeureux@gmail.com>
2024-11-19 21:14:26 +0100

    update 401 et 404 membre

--- html/401/index.php
+++ html/401/index.php
@@ -13,12 +13,12 @@
 
 <body class="min-h-screen flex flex-col">
     <div id="header"></div>
-    <main class="md:w-full mt-0 m-auto flex justify-between max-w-[1280px] p-2">
-        <div id="menu" class="relative md:block"></div>
-        <div class="text-center md:text-right">
+    <main class="md:w-full mt-0 m-auto flex max-w-[1280px] p-2">
+        <div id="menu" class="absolute md:block"></div>
+        <div class="m-auto text-center">
             <h1 class="font-cormorant text-[10rem]">401</h1>
             <p>Vous ne pouvez pas accéder à cette page.</p>
-            <img src="https://i.pinimg.com/originals/e0/5a/70/e05a70b23f36987ff395063a1e193db7.gif" class="mt-10 rounded-lg mx-auto md:ml-auto md:mr-0" alt="tottereau" width="250">
+            <img src="https://i.pinimg.com/originals/e0/5a/70/e05a70b23f36987ff395063a1e193db7.gif" class="mt-10 rounded-lg m-auto" alt="tottereau" width="250">
         </div>
     </main>
     <div id="footer" class=""></div>
