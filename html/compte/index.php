1f1df035b870df2a25980d883ddc8e4058296723
RatPeureux <unratpeureux@gmail.com>
2024-11-20 14:41:34 +0100

    fix: pb model leo

--- html/compte/index.php
+++ html/compte/index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="image" href="/public/images/favicon.png">
+    <link rel="stylesheet" href="/styles/output.css">
+    <title>Mon compte</title>
+    <script type="module" src="/scripts/main.js" defer></script>
+    <script type="module" src="/scripts/loadComponents.js" defer></script>
 </head>
-<body>
-    
+
+<body class="min-h-screen flex flex-col">
+    <div id="header"></div>
+    <div id="footer"></div>
 </body>
+
 </html>
