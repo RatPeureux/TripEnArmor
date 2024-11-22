cf2136caad54d0ffbfb88ffd1d830183ecb12619
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-22 13:47:36 +0100

    Modification de l'exemple + ajout du bouton

--- html/dev/index.php
+++ html/dev/index.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . "/../../controller/activite_controller.php";
 
 for ($id_activite = 0; $id_activite < 10; $id_activite++) {
-    require __DIR__ . "/../../view/activite_view.php";
+    require __DIR__ . "/../../view/activite_carte.php";
 }
+
+require __DIR__ . "/../../view/bouton.php";
 ?>
