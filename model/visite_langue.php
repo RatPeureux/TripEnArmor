74f6e5a1a1741c6f6da153da7b5fe5cd0718fc06
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-26 11:57:23 +0100

    feat: add methods for linking and unlinking visits and languages in VisiteLangueController

--- model/visite_langue.php
+++ model/visite_langue.php
@@ -4,25 +4,11 @@ class VisiteLangue extends BDD {
 
     private $nom_table = "sae_db._visite_langue";
 
-    static function createVisiteLangue($id_offre,$id_langue) {
-        $query = "INSERT INTO (id_offre, id_langue". self::$nom_table ."VALUES (?, ?) RETURNING *";
-        $statement = self::$db->prepare($query);
-        $statement->bindParam(1, $id_offre);
-        $statement->bindParam(2, $id_langue);
-        
-        if($statement->execute()){
-            return $statement->fetchAll(PDO::FETCH_ASSOC)[0];
-        } else {
-            echo "ERREUR: Impossible de créer la langue de visite";
-            return -1;
-        }
-    }
-
-    static function getVisiteLangueById($id){
+    function getLanguesBydIdVisite($id_offre){
         self::initBDD();
         $query = "SELECT * FROM " . self::$nom_table ." WHERE id_offre = ?";
         $statement = self::$db->prepare($query);
-        $statement->bindParam(1, $id);
+        $statement->bindParam(1, $id_offre);
 
         if ($statement->execute()){
             return $statement->fetchAll(PDO::FETCH_ASSOC)[0];
@@ -31,28 +17,56 @@ class VisiteLangue extends BDD {
             return false;
         }
     }
-    
-    static function updateVisiteLangue($id_offre, $id_langue) {
+
+    function getVisitesByIdLangue($id_langue){
         self::initBDD();
-        $query = "UPDATE " . self::$nom_table . " SET id_langue = ? WHERE id_offre = ? RETURNING *";
+        $query = "SELECT * FROM " . self::$nom_table ." WHERE id_langue = ?";
         $statement = self::$db->prepare($query);
         $statement->bindParam(1, $id_langue);
-        $statement->bindParam(7, $id_offre);
 
+        if ($statement->execute()){
+            return $statement->fetchAll(PDO::FETCH_ASSOC)[0];
+        } else {
+            echo "ERREUR";
+            return false;
+        }
+    }
+
+    function checkIfLinkExists($id_offre, $id_langue) {
+        self::initBDD();
+        $query = "SELECT * FROM ". self::$nom_table ." WHERE id_offre = ? AND id_langue = ?";
+
+        $statement = self::$db->prepare($query);
+        $statement->bindParam(1, $id_offre);
+        $statement->bindParam(2, $id_langue);
+
+        if ($statement->execute()){
+            return !empty($statement->fetchAll(PDO::FETCH_ASSOC)[0]);
+        } else {
+            return false;
+        }
+    }
+
+    function createVisiteLangue($id_offre,$id_langue) {
+        $query = "INSERT INTO (id_offre, id_langue". self::$nom_table ."VALUES (?, ?) RETURNING *";
+        $statement = self::$db->prepare($query);
+        $statement->bindParam(1, $id_offre);
+        $statement->bindParam(2, $id_langue);
+        
         if($statement->execute()){
             return $statement->fetchAll(PDO::FETCH_ASSOC)[0];
         } else {
-            echo "ERREUR: Impossible de mettre à jour la langue de la visite";
             return -1;
         }
     }
 
-    static function deleteVisiteLangue($id) {
+    function deleteVisiteLangue($id_offre, $id_langue) {
         self::initBDD();
         $query = "DELETE FROM". self::$nom_table ."WHERE id_offre = ?";
 
         $statement = self::$db->prepare($query);
-        $statement->bindParam(1, $id);
+        $statement->bindParam(1, $id_offre);
+        $statement->bindParam(2, $id_langue);
 
         return $statement->execute();
     }
