4fc027007bde0e1a52d5d530fa98684383ce20a4
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-28 10:19:21 +0100

    fix: initialize database connection in createProPrive method

--- model/pro_prive.php
+++ model/pro_prive.php
@@ -7,6 +7,7 @@ class ProPrive extends BDD {
     private $nom_table = "sae_db._pro_prive";
 
     static function createProPrive($email, $mdp, $tel, $adresseId, $nom_pro, $num_siren) {
+        self::initBDD();
         $query = "INSERT INTO (email, mdp_hash, num_tel, id_adresse, nom_pro, num_siren". self::$nom_table ."VALUES (?, ?, ?, ?, ?, ?) RETURNING id_compte";
         $statement = self::$db->prepare($query);
         $statement->bindParam(1, $email);
