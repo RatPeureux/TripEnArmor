4328be28b80760c4adca016adbf7af87f8ad74f0
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-11-29 11:57:15 +0100

    modified header

--- html/public/components/header.php
+++ html/public/components/header.php
@@ -18,21 +18,24 @@
             require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/php_files/authentification.php';
             if (isConnectedAsMember()) { ?>
                 <a href="/scripts/logout.php" class="flex flex-col items-center" onclick="return confirmLogout()">
-                     <div class="border border-primary rounded-lg p-2">
+                    <div class="border border-primary rounded-lg p-2">
                         <p>
                             Se déconnecter
                         </p>
-                     </div>
+                    </div>
+                </a>
+                <a href="/compte">
+                    <i class="text-3xl fa-regular fa-user"></i>
                 </a>
             <?php } else { ?>
                 <!-- Sinon si pas connecté -->
                 <a href="/connexion">
                     <!-- <i class="text-3xl fa-regular fa-user"></i> -->
-                     <div class="border border-primary rounded-lg p-2">
+                    <div class="border border-primary rounded-lg p-2">
                         <p>
                             Se connecter
                         </p>
-                     </div>
+                    </div>
                 </a>
             <?php } ?>
         </div>
