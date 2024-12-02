4b405587fb40cb4c9621a555d799946fce5e99b8
RatPeureux <unratpeureux@gmail.com>
2024-12-02 09:32:05 +0100

    fix: pb menu

--- html/public/components/menu-pro.php
+++ html/public/components/menu-pro.php
@@ -7,13 +7,13 @@
         ...
 -->
 <div class="h-full bg-base100 fixed top-0 w-1/4 left-0 -translate-x-full duration-200 z-50">
-    <div class="p-4 flex flex-row gap-3 justify-start items-center h-20 border-b-2 border-black">
+    <div class="p-4 flex flex-row gap-3 justify-start items-center h-20">
         <i class="text-3xl fa-solid fa-circle-xmark cursor-pointer" onclick="toggleMenu()"></i>
         <h1 class="text-h1">Menu</h1>
     </div>
     <div class="all-items flex flex-col items-stretch">
-        <a class="pl-5 py-3 border-b-2 border-black" href="/pro">Accueil</a>
-        <a class="pl-5 py-3 border-b-2 border-black" href="/pro">...</a>
+        <a class="pl-5 py-3 border-t-2 border-black" href="/pro">Accueil</a>
+        <a class="pl-5 py-3 border-t-2 border-black" href="/pro">...</a>
     </div>
 </div>
 <div id="layer-background" onclick="toggleMenu()" class="hidden fixed w-full h-full top-0 left-0 z-40"></div>
