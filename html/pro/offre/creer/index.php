e32b3eaa33b009efdcc35da3f4689964fdfb6f50
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-03 14:13:30 +0100

    added a peer

--- html/pro/offre/creer/index.php
+++ html/pro/offre/creer/index.php
@@ -596,7 +596,7 @@ $pro = verifyPro();
 										<div class="inline-flex items-center cursor-pointer space-x-2 peer"
 										onclick="toggleCheckbox('guide')">
 											<p>Visite guidée :</p>
-											<input type="checkbox" name="guide" id="guide" value="" class="sr-only">
+											<input type="checkbox" name="guide" id="guide" value="" class="sr-only peer">
 											<div
 												class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
 											</div>
