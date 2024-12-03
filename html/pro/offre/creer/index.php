48a4674e632aca4017a2dcdf1b1379825c5844fd
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-03 14:11:33 +0100

    tailwindcss change

--- html/pro/offre/creer/index.php
+++ html/pro/offre/creer/index.php
@@ -593,15 +593,15 @@ $pro = verifyPro();
 								<!-- Visite -->
 								<div class="flex justify-between items-center w-full space-x-2 optionVisite hidden">
 									<div class="inline-flex items-center space-x-4">
-										<div class="inline-flex items-center cursor-pointer space-x-2"
+										<div class="inline-flex items-center cursor-pointer space-x-2 peer"
 										onclick="toggleCheckbox('guide')">
 											<p>Visite guidée :</p>
-											<input type="checkbox" name="guide" id="guide" value="" class="sr-only peer">
+											<input type="checkbox" name="guide" id="guide" value="" class="sr-only">
 											<div
 												class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
 											</div>
 										</div>
-										<div class="space-x-2 w-fit flex items-center invisible peer-checked:visible">
+										<div class="space-x-2 w-fit flex items-center invisible peer:has-[:checked]:visible">
 											<p>
 												Langues parlées :
 											</p>
