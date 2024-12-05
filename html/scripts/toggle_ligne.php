1ba0336506b2f8114ec643615fd56613b4244d73
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-05 16:58:13 +0100

    new en ligne

--- html/scripts/toggle_ligne.php
+++ html/scripts/toggle_ligne.php
@@ -21,14 +21,10 @@ if (isset($_GET['id_offre'])) {
         $stmt->bindParam(':id_offre', $id_offre);
     }
     $stmt->execute();
-    $est_en_ligne = $est_en_ligne ? false : true;
 
-    $stmt = $dbh->prepare("INSERT INTO sae_db._log_changement_status (id_offre, enligne) VALUES (:id_offre, {($est_en_ligne ? 'false' : 'true')}) RETURNING enligne");
-    $stmt->bindParam(':id_offre', $id_offre);
+    $stmt = $dbh->prepare("INSERT INTO sae_db._log_changement_status (id_offre, enligne) VALUES (?, " . ($est_en_ligne ? 'false' : 'true') . "})");
+    $stmt->bindParam(1, $id_offre);
     $stmt->execute();
-    $enligne = $stmt->fetch(PDO::FETCH_ASSOC)[0]['enligne'];
-    
-    echo $enligne;
 }
 header('Location: /pro');
 exit();
