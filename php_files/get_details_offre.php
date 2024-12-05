fef84120495147f373ef617aa8e635161c0baff7
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-05 10:04:15 +0100

    var_dumping ids_repas

--- php_files/get_details_offre.php
+++ php_files/get_details_offre.php
@@ -109,6 +109,10 @@ if ($categorie_offre == 'restauration') {
     $stmt->execute();
     $ids_repas = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $tags = '';
+
+    echo "ids_repas : ";
+    var_dump($ids_repas);
+
     // Récup chaque nom de tag, et l'ajouter aux tags
     foreach ($ids_repas as $id_repas) {
         $stmt = $dbh->prepare("SELECT nom FROM sae_db._type_repas WHERE id_type_repas = :id_repas");
