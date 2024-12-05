1e6c20bffb801f7fa780d36842cb96218504d278
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-05 16:08:19 +0100

    var_dumping

--- html/scripts/toggle_ligne.php
+++ html/scripts/toggle_ligne.php
@@ -23,7 +23,9 @@ if (isset($_GET['id_offre'])) {
     $stmt->execute();
     $est_en_ligne = !$est_en_ligne;
 
-    $stmt = $dbh->prepare("INSERT INTO sae_db._log_changement_status (id_offre, enLigne) VALUES (:id_offre, :en_ligne)");
+    var_dump($est_en_ligne);
+
+    $stmt = $dbh->prepare("INSERT INTO sae_db._log_changement_status (id_offre, enligne) VALUES (:id_offre, :en_ligne)");
     $stmt->bindParam(':id_offre', $id_offre);
     $stmt->bindParam(':en_ligne', $est_en_ligne);
     $stmt->execute();
