7d197f00a7213c0e499b7e7874a5de7525419bb5
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2024-12-05 09:08:24 +0100

    tag_restaurant should work

--- model/tag_restaurant_restauration.php
+++ model/tag_restaurant_restauration.php
@@ -3,24 +3,7 @@ require_once dirname($_SERVER['DOCUMENT_ROOT']) . "/model/bdd.php";
 
 class tagRestaurantRestauration extends BDD
 {
-    static private $nom_table = "_tag_restaurant_restauration";
-
-    static function getByBothId($id_offre, $id_tag_restaurant){
-
-        self::initBDD();
-        $query = "SELECT * FROM " . self::$nom_table . "WHERE id_offre = ? AND id_tag_restaurant = ?";
-        $stmt = self::$db->prepare($query);
-
-        $stmt->bindParam(1, $id_offre);
-        $stmt->bindParam(2, $id_tag_restaurant);
-
-        if ($stmt->execute()) {
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }else {
-            echo "ERREUR : Impossible d'obtenir ce tag de ce restaurant";
-            return -1;
-        }
-    }
+    static private $nom_table = "sae_db._tag_restaurant_restauration";
 
     static function getByIdOffre($id_offre){
 
@@ -54,7 +37,49 @@ class tagRestaurantRestauration extends BDD
             echo "ERREUR : Impossible d'obtenir l'id du tag";
             return -1;
         }
+    }
 
+    static function checkIfLinkExists($id_restaurant, $id_tag)
+    {
+        self::initBDD();
+        $query = "SELECT * FROM " . self::$nom_table . " WHERE id_offre = ? AND id_tag = ?";
+
+        $statement = self::$db->prepare($query);
+        $statement->bindValue(1, $id_restaurant);
+        $statement->bindValue(2, $id_tag);
+
+        if ($statement->execute()) {
+            return count($statement->fetchAll(PDO::FETCH_ASSOC)) != 0;
+        } else {
+            return -1;
+        }
+    }
+
+    static function linkOffreAndTag($id_restaurant, $id_tag) {
+        self::initBDD();
+        $query = "INSERT INTO ". self::$nom_table . " (id_offre, id_tag_restaurant) VALUES (?, ?) RETURNING *";
+
+        $statement = self::$db->prepare($query);
+        $statement->bindValue(1, $id_restaurant);
+        $statement->bindValue(2, $id_tag);
+        
+        if ($statement->execute()) {
+            return $statement->fetchAll(PDO::FETCH_ASSOC)[0];
+        } else {
+            return -1;
+        }
+    }
+
+    static function unlinkOffreAndTag( $id_restaurant, $id_tag)
+    {
+        self::initBDD();
+        $query = "DELETE FROM ". self::$nom_table . " WHERE id_offre = ? AND id_tag_restaurant = ?";
+
+        $statement = self::$db->prepare($query);
+        $statement->bindValue(1, $id_restaurant);
+        $statement->bindValue(2, $id_tag);
+
+        return $statement->execute();
     }
 
 }
