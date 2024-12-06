aa47b522a985f86dba4b472cd1a58dac424fd66a
AntoineToullec <148762390+AntoineToullec@users.noreply.github.com>
2024-12-06 23:11:16 +0100

    Plus d'* devant la flèche

--- html/pro/connexion/index.php
+++ html/pro/connexion/index.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">*
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" type="image" href="/public/images/favicon.png">
     <link rel="stylesheet" href="/styles/input.css">
