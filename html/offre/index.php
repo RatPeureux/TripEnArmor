df9b88125a21a7b412ca5c3bc6f5ef6a1c4b62cd
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-13 10:07:47 +0100

    w-4

--- html/offre/index.php
+++ html/offre/index.php
@@ -367,15 +367,15 @@ session_start();
                                     <?php for ($i = 0; $i < 5; $i++) {
                                         if ($moyenne > 1) {
                                             ?>
-                                            <img class="w-5" src="/public/icones/oeuf_plein.svg" alt="1 point de note">
+                                            <img class="w-4" src="/public/icones/oeuf_plein.svg" alt="1 point de note">
                                             <?php
                                         } else if ($moyenne > 0) {
                                             ?>
-                                                <img class="w-5" src="/public/icones/oeuf_moitie.svg" alt="0.5 point de note">
+                                                <img class="w-4" src="/public/icones/oeuf_moitie.svg" alt="0.5 point de note">
                                             <?php
                                         } else {
                                             ?>
-                                                <img class="w-5" src="/public/icones/oeuf_vide.svg" alt="0 point de note">
+                                                <img class="w-4" src="/public/icones/oeuf_vide.svg" alt="0 point de note">
                                             <?php
                                         }
                                         $moyenne--;
