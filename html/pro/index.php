c75c5fdcac35d2c94a0a31ae7714b66b6be36ed2
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-14 11:05:16 +0100

    refactor layout in index.php to change main container to flex and adjust border class for notifications section

--- html/pro/index.php
+++ html/pro/index.php
@@ -119,9 +119,9 @@ if (!function_exists('chaineVersMot')) {
     }
     ?>
 
-    <main class="mx-10 self-center w-full grow p-2">
+    <main class="mx-10 self-center w-full grow p-2 flex">
         <!-- TOUTES LES OFFRES (offre & détails) -->
-        <div class="w-full max-w-7xl grow tablette p-4 flex flex-col">
+        <div class="w-full max-w-7xl tablette p-4 flex flex-col">
 
             <!-- Conteneur des tags (!!! RECHERCHE) -->
             <div class="flex flex-wrap gap-4" id="tags-container"></div>
@@ -180,7 +180,7 @@ if (!function_exists('chaineVersMot')) {
             <?php } ?>
 
         </div>
-        <div class="inline-block w-full h-full border border-primary">
+        <div class="w-full h-full grow border border-primary">
             <h2 class="text-h2">
                 Notifications
             </h2>
