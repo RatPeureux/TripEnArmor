1a319874b63f83d662af1dcae61110f34ebd3940
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-14 14:11:20 +0100

    refactor CSS by removing unused classes and improve notification display logic

--- view/notification_view.php
+++ view/notification_view.php
@@ -1,27 +1,31 @@
 <?php
 require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/controller/avis_controller.php';
+require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/controller/membre_controller.php';
 
 $avisController = new avisController();
+$membreController = new MembreController();
 
 $avis = $avisController->getAvisByIdPro($_SESSION['id_pro']);
 
 if ($avis && count($avis) !== 0) {
     foreach($avis as $avi) {
-        print_r($avi);
+        // print_r($avi);
 ?>
 <div class="h-full p-2">
     <!-- lien vers l'offre -->
     <div class="w-full flex justify-between items-center">
-        <h3 class="text-gray-600"><span class="text-black">Titre</span> posté par
-            <span class="text-black">Auteur</span> Il y a <span class="text-black">Date de publication</span>
+        <h3 class="text-gray-600"><span class="text-black"><?php echo $avi['titre']; ?></span> posté par
+            <span class="text-black"><?php echo $membreController->getInfosMembre($avi['id_membre'])['pseudo']; ?></span> Il y a <span class="text-black"><?php echo $avi['date_publication']; ?></span>
         </h3>
         <div class="h-6 w-24 bg-primary">
             Note
         </div>
     </div>
-    <p>Vécu le Date de passage</p>
+    <p>Vécu le <?php echo $avi['date_experience']; ?></p>
     <p>
-        Description
+        <?php
+            echo $avi['commentaire']
+        ?>
     </p>
     <hr />
 </div>
