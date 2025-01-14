2e9d7128edb23fb4840c19e3ea6cfa89a4b95a37
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-14 09:08:32 +0100

    back to one css file

--- html/compte/profil/avis/index.php
+++ html/compte/profil/avis/index.php
@@ -29,10 +29,7 @@ $membre = verifyMember();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" type="image" href="/public/images/favicon.png">
-    <link rel="stylesheet" href="/styles/input.css">
-
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="/styles/config.js"></script>
+    <link rel="stylesheet" href="/styles/style.css">
     <script type="module" src="/scripts/main.js" defer></script>
     <script src="https://kit.fontawesome.com/d815dd872f.js" crossorigin="anonymous"></script>
 
