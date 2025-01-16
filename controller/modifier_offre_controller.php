7b1a9897d39bdf2b1760de3481498ddd9c9e42a4
richard <zenpoxa@gmail.com>
2025-01-16 18:25:33 +0100

    fix : quand offre modifiée et passée hors ligne, clore la période en ligne

--- controller/modifier_offre_controller.php
+++ controller/modifier_offre_controller.php
@@ -18,9 +18,16 @@ class ModifierOffreController
         return $offre;
     }
 
-    public function updateOffre($id, $titre, $description, $resume, $prix_mini, $date_creation, $date_mise_a_jour, $date_suppression, $est_en_ligne, $id_type_offre, $id_pro, $id_adresse, $accessibilite) {
+    public function updateOffre($id, $titre, $description, $resume, $prix_mini, $date_creation, $date_mise_a_jour, $date_suppression, $est_en_ligne, $id_type_offre, $id_pro, $id_adresse, $accessibilite)
+    {
         $updatedOffreId = $this->model::updateOffre($id, $titre, $description, $resume, $prix_mini, $date_creation, $date_mise_a_jour, $date_suppression, $est_en_ligne, $id_type_offre, $id_pro, $id_adresse, $accessibilite);
         $this->model::log("L'offre $id a été modifiée.");
+
+        // Si l'offre est passée hors ligne. Clore toute période 'en_ligne' en cours.
+        require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/controller/periodes_en_ligne_controller.php';
+        $periodes_en_ligne_controller = new PeriodesEnLigneController();
+        $periodes_en_ligne_controller->clorePeriodeByIdOffre($id_offre);
+
         return $updatedOffreId;
     }
 }
