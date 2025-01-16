ad6b13ff70f0358ca9b28cddf5dfc3870e9101c1
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-16 18:18:11 +0100

    Add console logs for child elements in optionToggler.js to aid debugging

--- html/scripts/optionToggler.js
+++ html/scripts/optionToggler.js
@@ -31,6 +31,7 @@ const show = (classToShow) => {
         option.classList.remove("hidden");
 
         const childElements = option.querySelectorAll('input:not([id*="horaires"]):not([id="newPrixName"]):not([id="newPrixValeur"]):not([id="newPrestationName"]):not([type="checkbox"]):not([id="duration"]):not([id="start_date"]), textarea');
+
         childElements?.forEach(child => {
             console.log(child)
             child.required = true;
@@ -46,7 +47,9 @@ const hide = () => {
             option.classList.add('hidden');
 
             const childElements = option.querySelectorAll('input:not([id*="horaires"]):not([id="newPrixName"]):not([id="newPrixValeur"]):not([id="newPrestationName"]):not([type="checkbox"]):not([id="duration"]):not([id="start_date"]), textarea');
+
             childElements?.forEach(child => {
+                console.log(child);
                 child.required = false;
             });
         }
