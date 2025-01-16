16cd6be2a3e067734c248aa0f23a524a4afed5eb
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-16 18:16:49 +0100

    Update optionToggler.js to refine child element selection by excluding additional IDs

--- html/scripts/optionToggler.js
+++ html/scripts/optionToggler.js
@@ -45,7 +45,7 @@ const hide = () => {
         for (const option of options) {
             option.classList.add('hidden');
 
-            const childElements = option.querySelectorAll('input:not([type="checkbox"]), textarea');
+            const childElements = option.querySelectorAll('input:not([id*="horaires"]):not([id="newPrixName"]):not([id="newPrixValeur"]):not([id="newPrestationName"]):not([type="checkbox"]):not([id="duration"]):not([id="start_date"]), textarea');
             childElements?.forEach(child => {
                 child.required = false;
             });
