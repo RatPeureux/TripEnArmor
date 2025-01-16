0a2ffbc28b220785eab4bff5063559e5f3cff8f7
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-01-16 18:13:36 +0100

    Update index.php and style.css to change SVG stroke class to 'stroke-rouge-logo'

--- html/pro/offre/creer/index.php
+++ html/pro/offre/creer/index.php
@@ -843,9 +843,9 @@ $pro = verifyPro();
 												<td class="w-fit group">
 													<input type="checkbox" id="newPrestationInclude" class="hidden peer">
 													<label for="newPrestationInclude"
-														class="h-max w-full cursor-pointer flex justify-center items-center fill-rouge-logo peer-checked:hidden">
+														class="h-max w-full cursor-pointer flex justify-center items-center stroke-rouge-logo peer-checked:hidden">
 														<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-															viewBox="0 0 32 32" fill="none" stroke="currentcardColor"
+															viewBox="0 0 32 32"
 															stroke-width="2.5" stroke-linecap="round"
 															stroke-linejoin="round" class="lucide lucide-square-x">
 															<rect width="28" height="28" x="2" y="2" rx="4" ry="4" />
