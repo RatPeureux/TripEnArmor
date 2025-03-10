0f858b090c3e895567e9877ba76406911a19b0ae
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-10 09:36:42 +0100

    refactor: remove database connection code and add phpinfo() function

--- html/dev/index.php
+++ html/dev/index.php
@@ -1,6 +1,3 @@
 <?php
-try {
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=nom_de_la_base;user=utilisateur;password=mot_de_passe');
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
+phpinfo();
+?>
