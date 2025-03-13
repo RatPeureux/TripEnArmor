0745a2c92e14ab18bcacff02c570dff61d17a74c
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-13 09:24:36 +0100

    first try for a map

--- html/dev/index.php
+++ html/dev/index.php
@@ -1,3 +1,21 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <title>Test Carte Interactive</title>
+</head>
+
+<body>
+    <div id="map" class="h-[200px] w-[400px]"></div>
+    <script src="./map.js" defer></script>
+</body>
+
+</html>
