28fabd2844bb509dcd7844256bb0d7d1065e037f
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-13 09:25:50 +0100

    use an inline script instead of a file

--- html/dev/index.php
+++ html/dev/index.php
@@ -15,7 +15,14 @@
 
 <body>
     <div id="map" class="h-[200px] w-[400px]"></div>
-    <script src="./map.js" defer></script>
+    <script>
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    </script>
 </body>
 
 </html>
