b5cbdd53311fba55172d4fdb32a58b7c35ea92bd
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-28 14:22:38 +0100

    fix: correct comment typo and remove unnecessary redirection

--- php_files/get_details_offre.php
+++ php_files/get_details_offre.php
@@ -42,13 +42,8 @@ $est_en_ligne = $offre['est_en_ligne'];
 $date_mise_a_jour = $offre['date_mise_a_jour'];
 $titre_offre = $offre['titre'];
 
-if (!$est_en_ligne) {
-    header('location: /401');
-    exit();
-}
 
-
-// Otenir la moyenne des notes de l'offre
+// Obtenir la moyenne des notes de l'offre
 $stmt = $dbh->prepare("SELECT avg, count FROM sae_db.vue_moyenne WHERE id_offre = :id_offre");
 $stmt->bindParam(':id_offre', $id_offre);
 $stmt->execute();
